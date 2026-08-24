ee734a6151aa93d32cdf23183ced4f24df16fbbe
harzeezco <wakilabdulazeez@gmail.com>
2026-08-24 18:58:23 +0100

    fix bug

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-                   "https://aesthetic-tiramisu-feb2c3.netlify.app, https://quadriismail.com", "https://quadriismail.com/"],
+                   "https://aesthetic-tiramisu-feb2c3.netlify.app", "https://quadriismail.com", "https://quadriismail.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
