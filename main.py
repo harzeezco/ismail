63f580ed211b1f78ab279ded6af146c413baed67
harzeezco <wakilabdulazeez@gmail.com>
2026-08-24 18:06:59 +0100

    fix bug

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://aesthetic-tiramisu-feb2c3.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
